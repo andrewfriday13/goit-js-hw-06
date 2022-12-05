b744a80f50f76e511ccf05c269aaec1ece49a1e9
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-05 18:36:53 +0100

    add second part

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const categoriesEl = document.getElementById('ingredients')
+
+
+ingredients.map((element) => {
+
+  const newIndrEl = document.createElement("li")
+
+  newIndrEl.classList.add('item')
+  newIndrEl.textContent = element
+
+ console.log(newIndrEl)
+ categoriesEl.appendChild(newIndrEl )
+}
+ )
