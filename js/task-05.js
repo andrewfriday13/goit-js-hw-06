87e5ee161e91dfa193d3e8c64af90399d1675f07
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-08 20:20:39 +0100

    add 5 part

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const inputId = document.getElementById('name-input');
 const nameUser = document.getElementById('name-output');
 
 inputId.addEventListener("input", (event) => {
-  nameUser.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    nameUser.textContent = "Anonymous"
+  } else {
+    nameUser.textContent = event.currentTarget.value;
+  }
 } )
