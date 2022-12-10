69e446b7a45a4d50a0cc47280f6d41bf73068081
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-10 12:12:57 +0100

    fix 3 part

--- js/task-03.js
+++ js/task-03.js
@@ -16,27 +16,7 @@ const images = [
 
 const gallery = document.querySelector('.gallery');
 
-const img = images.map(({ url, alt }) => {
-  const list = document.createElement('li')
-  const newImg = document.createElement('img')
-   newImg.alt = alt
-  newImg.src = url  
-  newImg.width = 300
-  list.append(newImg)
-  console.log(list)
-  // const listImg = gallery.insertAdjacentHTML("beforebegin", `<li><img class="animal__img" src="${url}" alt="${alt}" width= 300> </li> `)
+const img = images.map(({ url, alt }) => `<li><img class="animal__img" src="${url}" alt="${alt}" width= 300> </li> `).join("")
+  const listImg = gallery.insertAdjacentHTML("beforebegin", img)
   
-  return list
-})
-gallery.append(...img)
 
-
-// const newImg = document.createElement("img")
-  // newImg.alt = alt
-  // newImg.src = url  
-  // newImg.width =300
-
-
-  // listImg.appendChild(newImg)
-
-  // gallery.appendChild(listImg) 
